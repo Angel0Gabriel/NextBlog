0f8ef933e0e5ef6be1c836c952830929ad0689bb
Ângelo Gabriel <angelobielmoreira@hotmail.com>
2026-03-22 19:03:19 -0300

    add metadata for SEO and open graph configuration; include og-image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,27 @@
 import { LandingPage } from "@/templates/landing-page/landing-page";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Site.set",
+  description: "Venda seus produtos como afiliado em um único lugar",
+  robots: "index, follow",
+  openGraph: {
+    title: "Site.set",
+    description: "Venda seus produtos como afiliado em um único lugar",
+    // url: 'URL_PRODUCAO'
+    siteName: "Site.set",
+    locale: "pt_BR",
+    type: "website",
+    // images: [
+    //   {
+    //     url: 'URL_PRODUCAO',
+    //     width: 800,
+    //     height: 600,
+    //     alt: 'Site.set'
+    //   }
+    // ]
+  },
+};
 
 export default function HomePage() {
   return <LandingPage />;
